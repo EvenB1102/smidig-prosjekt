9a5c1c08ed2e2e028763ff036d619dfa690b216c
Ollybolli <Oliver_smil@hotmail.com>
2022-05-29 19:11:46 +0200

    Footer almost finished

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer.css"
+import "./Footer.css";
 
 const Footer = () => {
     return(
@@ -7,24 +7,49 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     {/*Column1 */}
-                    <div className="col">
-                        <h4>Navigate</h4>
-                        <ul className="list-unstyled">
-                           <li>Home</li>
-                           <li>About</li>
-                           <li>Contact</li>
-                        </ul>
-                        <vl></vl>
+                    <div className="col1">
+                        <p style={{fontFamily: "Euclid Circular B", fontSize: "36px", fontStyle: "normal", lineHeight: "120%", fontWeight: "400", position: "relative", left: "60%", top: "15%"}}>Navigate</p>
+                        <div className="list1">
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>Home</button>
+                            <br></br><br></br>
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>About</button>
+                            <br></br><br></br>
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>Contact</button>
+                            
+                        </div>
+                        <div classname="divider"></div>
+                       
+                        
+                        
                     </div>
+                    
+                    <div className="col2">
+                        <div className="list2">
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>Questions</button>
+                            <br></br><br></br>
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>Terms & Conditions</button>
+                            <br></br><br></br>
+                            <button style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "italic", lineHeight: "120%", fontWeight: "300",color: "#001826",border: "none",backgroundColor: "white"}}>Lorem ipsum</button>
+                        </div>
+                        </div>
+                    
+                    
                     {/*Column2 */}
-                    <div className="col">
-                        <image src="https://cdn.logo.com/hotlink-ok/logo-social.png"></image>
-                        <h4>Meloria impact</h4>
+                    <div className="col3">
+                    <img style={{position: "relative", top: "30%"}} src={require('../images/logo.png')} alt="logo"/>
+                        <p style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "normal", lineHeight: "120%", fontWeight: "400", position: "relative", left: "0%", color: "#001826", top: "20%"}}>Meliora Impact</p>
                         
                     </div>
                     {/*Column3 */}
-                    <div className="col">
-                        <h6>Sign up for the newsletter</h6>
+                    <div className="col3">
+                    <p style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "normal", lineHeight: "120%", fontWeight: "400", position: "relative", left: "0%", color: "#001826", top: "20%"}}>Sign up for the Newsletter</p>
+                    <p style={{fontFamily: "Euclid Circular B", fontSize: "24px", fontStyle: "normal", lineHeight: "120%", fontWeight: "400", position: "relative", right: "25%", color: "#001826", top: "25%"}}>Email:</p>
+                    <input style={{width: "45%", height: "14%", position: "relative",left: "6%", top: "4%", background: "#ECECEC", border: "none", borderRadius:"15px" }}></input>
+                        <div style={{position: "relative", top:"12%", left: "5%"}} classname="social-media-div">
+                            <img style={{position: "relative",  width: "13%", margin:"20px"}} src={require('../images/twitter.png')} alt="logo"/>
+                            <img style={{position: "relative",  width: "13%", margin:"20px"}} src={require('../images/instagram.png')} alt="logo"/>
+                            <img style={{position: "relative",  width: "13%", margin:"20px"}} src={require('../images/facebook.png')} alt="logo"/>
+                        </div>
                     </div>
                     
                 </div>
