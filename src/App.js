2375e6d9a308eafd352455b0bdbaaf7dd198c4a3
ZillaCoop <cooprix1@gmail.com>
2022-05-31 12:28:36 +0200

    Squashed commit of the following:
    commit bfed00e7858f683ff143ebc6a2e978370f950089
    Author: EvenB1102 <72261373+EvenB1102@users.noreply.github.com>
    Date:   Tue May 24 14:23:58 2022 +0200
    
        Navbar
    
        Made navbar with react js
    
    commit 7dbb47d8db291f24d92492fb7a33344eaf326ff2
    Author: EvenB1102 <72261373+EvenB1102@users.noreply.github.com>
    Date:   Thu May 12 08:03:26 2022 +0200
    
        Revert "Revert "added prettier""
    
        This reverts commit 47af621a7b87259987ac22afb420c05a6d4263ef.
    
    commit 47af621a7b87259987ac22afb420c05a6d4263ef
    Author: EvenB1102 <72261373+EvenB1102@users.noreply.github.com>
    Date:   Thu May 12 08:03:19 2022 +0200
    
        Revert "added prettier"
    
        This reverts commit e6f37deaba7bfb741c2ba3c10c2233284017f139.
    
    commit e6f37deaba7bfb741c2ba3c10c2233284017f139
    Author: EvenB1102 <72261373+EvenB1102@users.noreply.github.com>
    Date:   Wed May 11 12:58:26 2022 +0200
    
        added prettier
    
        Added dependency prettier and prettier format

--- src/App.js
+++ src/App.js
@@ -8,12 +8,30 @@ import ExplorationPage from "./pages/ExplorationPage";
 import DonationPage from "./pages/DonationPage";
 
 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './Components/Pages/Home';
+import Explore from './Components/Pages/Explore';
+import Resources from './Components/Pages/Resources';
+import News from './Components/Pages/News';
 
 
 
 function App() {
   return (
-    <DonationPage />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home}/>
+        <Route path='/Explore' exact component={Explore}/>
+        <Route path='/Resources' exact component={Resources}/>
+        <Route path='/News' exact component={News}/>
+      </Routes>
+    </Router>
+    
+    
   );
 }
 
