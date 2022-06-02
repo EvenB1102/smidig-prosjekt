5382a5b720c96358dd7e725947b62eac9aab1b5d
ZillaCoop <70030526+ZillaCoop@users.noreply.github.com>
2022-06-02 12:48:50 +0200

    OrganizationPage commit (#19)
    Co-authored-by: Shahid Ghazanfar <71063599+Gazza7d3@users.noreply.github.com>

--- src/Components/HowWeWork.js
+++ src/Components/HowWeWork.js
@@ -1,7 +1,11 @@
 import Work from "../css/Work.css"
+import Vector1 from '../images/Vector1.png'
+import Vector2 from '../images/Vector2.png'
+import Vector3 from '../images/Vector3.png'
+import Vector4 from '../images/Vector4.png'
 
 
-const HowWeWork = ({text}) => {
+const HowWeWork = () => {
     return (
         <div className="work-section">
             <div className="work-container">
@@ -10,11 +14,14 @@ const HowWeWork = ({text}) => {
                         <h3>How We Work</h3>
                     </div>
                     <div className="work-content">
-                        <p>{text}</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
-                <div className="work-image-section">
-                    <img src="" alt="Organization"></img>
+                <div className="work-image-section" style={{display: "flex", flexDirection: "row", width: "70px", height: "70px"}}>
+                    <img src={Vector1} alt=""></img>&nbsp;&nbsp;&nbsp;
+                    <img src={Vector2} alt=""></img>&nbsp;&nbsp;&nbsp;
+                    <img src={Vector3} alt=""></img>&nbsp;&nbsp;&nbsp;
+                    <img src={Vector4} alt=""></img>
                 </div>
             </div>
         </div>
