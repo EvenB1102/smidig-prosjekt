34c1549002c6ec9707e7fd72312369f0ba03903a
Shahid Ghazanfar <71063599+Gazza7d3@users.noreply.github.com>
2022-06-09 20:17:38 +0200

    fix setting layout

--- src/Components/SettingPage/SettingsLeft.js
+++ src/Components/SettingPage/SettingsLeft.js
@@ -3,17 +3,17 @@ import PngWing from './SettingsPageImg/PngWing.png'
 
 const SettingsLeft = () => {
   return (
-    <div style={{width: "50%", padding: 0}}>
-        <div style={{ border: "solid 1px #C4C4C4" }}><h1 style={{paddingLeft: "2rem"}}>Settings</h1></div>
+    <div style={{width: "50%"}}>
+        <div style={{ border: "solid 1px #C4C4C4", paddingLeft:"50px" }}><h1 style={{paddingLeft: "2rem"}}>Settings</h1></div>
         
-        <div className='youraccount' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}><h2 style={{marginLeft: "2em"}}>Your account</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Security and account access</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Privacy and safety</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Notifications</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Accessibility, display, and languages</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Additional resources</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between"}}><h2 style={{marginLeft: "2em"}}>Subscription</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
-
+        <div className='youraccount' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Your account</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Security and account access</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Privacy and safety</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Notifications</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Accessibility, display, and languages</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Additional resources</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        <div className='activediv' style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between",  paddingleft: "50", paddingRight: "50px" }}><h2 style={{marginLeft: "2em"}}>Subscription</h2><img style={{ height: "25px", width: "25px" }} src={PngWing} alt=""/></div>
+        
         
 
 
--- src/Components/SettingPage/SettingsPage.js
+++ src/Components/SettingPage/SettingsPage.js
@@ -7,7 +7,7 @@ import SettingsRight from './SettingsRight'
 const SettingsPage = () => {
   return (
     <div className='settingspage'>
-    <div style={{ display: "flex", flexDirection: "row", alignItems: "flexStart", flexFlow: "flow", minHeight: "695px", padding: 0 }}>
+    <div style={{ display: "flex", flexDirection: "row", alignItems: "flexStart", flexFlow: "flow", minHeight: "695px", }}>
       <SettingsLeft/>
       <SettingsRight/>
     </div>
