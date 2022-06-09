64d1e597bdc0b1513246111a47b6e0af733bb336
Shahid Ghazanfar <71063599+Gazza7d3@users.noreply.github.com>
2022-06-09 19:45:43 +0200

    minor fix

--- src/Components/AboutPage/About2Frame.js
+++ src/Components/AboutPage/About2Frame.js
@@ -6,7 +6,7 @@ const About2Frame = () => {
     <div style={{display: "flex", flex: 2, justifyContent: "space-between", padding: "0px 45px"  }}>
         <img src={grow} alt="Failed to find Grow image" />
         <div style= {{display: "flex", flexDirection: "column", flexWrap: "wrap"}}>
-            <h5 style={{color: "#709E9B"}}>Our Mission</h5>
+            <h5 style={{color: "#709E9B"}}>Our Vision</h5>
             <h1 >Be the chance to make a difference</h1>
             <p style={{color: "#8B8B8B"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elit, quam suscipit purus donec amet. Egestas volutpat facilisi eu libero.
 
